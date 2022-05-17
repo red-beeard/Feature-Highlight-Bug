c92b341ff9b68934d83d540b980518452f5a8ce5
Alexander Nifontov <nifon1998@yandex.ru>
2022-05-17 23:59:12 +0300

    Created situation

--- FeatureHighlightBug/MainViewController.swift
+++ FeatureHighlightBug/MainViewController.swift
@@ -6,30 +6,94 @@
 //
 
 import UIKit
+import MaterialComponents
 
 class MainViewController: UIViewController {
 	
-	@IBOutlet var indigoView: UIView!
-	@IBOutlet var orangeView: UIView!
-	@IBOutlet var tealView: UIView!
+	private let indigoView = UIView()
+	private let orangeView = UIView()
+	private let tealView = UIView()
+	
+	private let buttonStartHighlight = UIButton(type: .system)
 	
 	private lazy var views = [indigoView, orangeView, tealView]
 	
+	override func loadView() {
+		super.loadView()
+		
+		let stack = UIStackView(arrangedSubviews: views)
+		stack.spacing = 15
+		stack.axis = .vertical
+		stack.alignment = .fill
+		stack.distribution = .fillEqually
+		
+		stack.translatesAutoresizingMaskIntoConstraints = false
+		buttonStartHighlight.translatesAutoresizingMaskIntoConstraints = false
+		
+		view.addSubview(stack)
+		view.addSubview(buttonStartHighlight)
+		
+		NSLayoutConstraint.activate([
+			indigoView.heightAnchor.constraint(equalTo: indigoView.widthAnchor, multiplier: 1),
+			
+			stack.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.6),
+			stack.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -20),
+			stack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+			
+			buttonStartHighlight.topAnchor.constraint(equalTo: stack.bottomAnchor, constant: 40),
+			buttonStartHighlight.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+		])
+	}
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		
+		buttonStartHighlight.setTitle("Start Highlighted", for: .normal)
+		
+		view.backgroundColor = .systemBackground
+		indigoView.backgroundColor = .systemIndigo
+		orangeView.backgroundColor = .systemOrange
+		tealView.backgroundColor = .systemTeal
+		buttonStartHighlight.backgroundColor = .systemBlue
+		buttonStartHighlight.setTitleColor(.white, for: .normal)
+		
+		indigoView.layer.cornerRadius = 15
+		orangeView.layer.cornerRadius = 15
+		tealView.layer.cornerRadius = 15
+		buttonStartHighlight.layer.cornerRadius = 5
+		
+		buttonStartHighlight.addTarget(self, action: #selector(buttonStartHighlight_Click), for: .touchUpInside)
+	}
+	
+	private func getTitle(for view: UIView) -> String {
+		switch view {
+			case indigoView: return "Indigo"
+			case orangeView: return "Orange"
+			case tealView: return "Teal"
+			default: return "nil"
+		}
+	}
+	
+	private func showHighlight(_ views: [UIView?]) {
+		guard let view = views.first as? UIView else { return }
+		
+		let vc = MaterialComponents.MDCFeatureHighlightViewController(highlightedView: view) { [weak self] _ in
+			guard let self = self else { return }
+			let highlightedViews = Array(views.dropFirst())
+			self.showHighlight(highlightedViews)
+		}
+		
+		vc.titleText = getTitle(for: view)
+		vc.titleColor = .white
+		vc.titleFont = .systemFont(ofSize: 17, weight: .semibold)
+		vc.outerHighlightColor = view.backgroundColor
+		vc.view.backgroundColor = .clear
+		
+		present(vc, animated: true)
 	}
 
-	@IBAction func startHighlight_Click(_ sender: UIButton) {
-		let vc = MDCFeatureHighlightViewController(highlightedView: highlightedView) { [weak self] _ in
-			guard let self = self else { return }
-			
-			self.secureAppSettings.discoveredFeatures.insert(feature)
-			self.secureAppSettings.save()
-			
-			let featuresToShow = Array(features.dropFirst())
-			// Recursion
-			self.showFeatures(featuresToShow)
-		}
+	@objc private func buttonStartHighlight_Click(_ sender: UIButton) {
+		showHighlight(views)
 	}
 	
 }
---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '14.0'
+
+target 'FeatureHighlightBug' do
+  pod 'MaterialComponents/FeatureHighlight'
+end
+
